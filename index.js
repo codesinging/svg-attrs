aaebd7fe8c9ce6242ac886eebaf256a626efdadd
codesinging <codesinging@gmail.com>
2020-12-28 14:25:53 +0800

    add function string()

--- index.js
+++ index.js
@@ -5,37 +5,46 @@ const lowercase = name => {
     return name.toLowerCase()
 }
 
+const parse = (data, callback) => {
+    parseString(data, {
+        strict: false,
+        attrkey: 'ATTR',
+        attrNameProcessors: [lowercase],
+    }, (err, result) => {
+        if (err){
+            return callback(err)
+        }
+        let width = 0
+        let height = 0
+        if (result.SVG.ATTR.width && result.SVG.ATTR.height){
+            width = result.SVG.ATTR.width
+            height = result.SVG.ATTR.height
+        } else {
+            let viewbox = result.SVG.ATTR.viewbox.split(/\s+/)
+            width = viewbox[2]
+            height = viewbox[3]
+        }
+        callback(null, {
+            width: parseFloat(width),
+            height: parseFloat(height),
+            attrs: result.SVG.ATTR
+        })
+    })
+}
+
+const string = (data, callback) => {
+    return parse(data, callback)
+}
+
 const file = (path, callback) => {
     fs.readFile(path, {encoding: 'utf8'}, (error, data) => {
         if (error){
             return callback(error)
         }
 
-        parseString(data, {
-            strict: false,
-            attrkey: 'ATTR',
-            attrNameProcessors: [lowercase],
-        }, (err, result) => {
-            if (err){
-                return callback(err)
-            }
-            let width = 0
-            let height = 0
-            if (result.SVG.ATTR.width && result.SVG.ATTR.height){
-                width = result.SVG.ATTR.width
-                height = result.SVG.ATTR.height
-            } else {
-                let viewbox = result.SVG.ATTR.viewbox.split(/\s+/)
-                width = viewbox[2]
-                height = viewbox[3]
-            }
-            callback(null, {
-                width: parseFloat(width),
-                height: parseFloat(height),
-                attrs: result.SVG.ATTR
-            })
-        })
+        return parse(data, callback)
     })
 }
 
+module.exports.string = string
 module.exports.file = file
